e193d02f0cba365ee82cecac77e1db10e0548048
Mattias Axelsson <mattias.axelsson@happiness.se>
2015-12-02 23:27:22 +0100

    CocaCola-1.0

--- CocaCola/README.md
+++ CocaCola/README.md
@@ -1,3 +1,3 @@
 # CocaCola
 Adds CocaCola to PocketMine!
-Thirst stops here... Have a Coke. Get health or die of deliciousness overload.
+Thirst stops here... Have a Coke. Get health + food or die of deliciousness overload.
--- CocaCola/src/Acke/CocaCola/CocaCola.php
+++ CocaCola/src/Acke/CocaCola/CocaCola.php
@@ -12,32 +12,46 @@ use pocketmine\Player;
 
 class CocaCola extends PluginBase{
 
-    public function onEnable(){
-        $this->saveDefaultConfig();
-    }
+		public function onEnable(){
+				$this->saveDefaultConfig();
+		}
 
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-        if(strtolower($command->getName()) == "cocacola"){
-            if($sender instanceof Player){
-            	if(!($sender->hasPermission("cocacola") || $sender->hasPermission("cocacola.command") || $sender->hasPermission("cocacola.command.me"))) {
-                    return false;
-    	    	}
-            	if(mt_rand(1, 20) == 1){
-            	    $sender->setHealth(0);
-        	    $sender->sendMessage(TextFormat::RED."Whops! Deliciousness overload...");
-      	    	}
-      	    	else{
-			        	    $sender->setHealth($sender->getHealth() +2);
-                    $sender->sendMessage(TextFormat::GREEN."Pure refreshment! Health restored +2.");
-                    foreach($this->getConfig()->get("commands") as $command){
-                    	$this->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{player}", $sender->getName(), $command));
-                    }
-            	}
-            }
-            else{
-            	$sender->sendMessage(TextFormat::RED."N00b! Run this command in-game!");
-            }
-            return true;
-        }
-    }
+		public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+				if(strtolower($command->getName()) == "cocacola"){
+						if($sender instanceof Player){
+								if(!($sender->hasPermission("cocacola") || $sender->hasPermission("cocacola.command") || $sender->hasPermission("cocacola.command.me"))) {
+										return false;
+								}
+						if(mt_rand(1, 20) == 1){
+								$sender->setHealth(0);
+								$sender->sendMessage(TextFormat::RED."Whops! Deliciousness overload...");
+						}
+						else{
+								if($sender->getHealth() == 19){
+								    $sender->setHealth(20);
+								    $sender->sendMessage(TextFormat::GREEN."Pure refreshment! Full health!");
+							  }
+								if($sender->getHealth() <= 18){
+										$sender->setHealth($sender->getHealth() + 2);
+										$sender->sendMessage(TextFormat::GREEN."Pure refreshment! Health +2.");
+								}
+								if($sender->getFood() == 19){
+  							    $sender->setFood(20);
+									  $sender->sendMessage(TextFormat::GREEN."Ahhh! Thirst Stops Here.");
+								}
+								if($sender->getFood() <= 18){
+									  $sender->setFood($sender->getFood() + 2);
+									  $sender->sendMessage(TextFormat::GREEN."Ahhh! Food +2.");
+								}
+								foreach($this->getConfig()->get("commands") as $command){
+										$this->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{player}", $sender->getName(), $command));
+								}
+						}
+				}
+				else{
+					$sender->sendMessage(TextFormat::RED."N00b! Run this command in-game!");
+				}
+				return true;
+				}
+		}
 }
